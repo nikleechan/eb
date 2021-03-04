202e966e73756be77dcde2560d14760a85c9c664
nikleechan <nikleechan@gmail.com>
2021-03-04 22:49:25 +0800

    1

---
+++ embyonekey.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+cd /var/packages/EmbyServer/target/mono/bin
+curl https://raw.githubusercontent.com/nikleechan/eb/master/mb3admin.com.cert.pem >> /etc/ssl/certs/ca-certificates.crt
+./cert-sync /etc/ssl/certs/ca-certificates.crt
+cd /usr/local/etc/nginx/conf.d/*-*-*-*
+wget https://raw.githubusercontent.com/nikleechan/eb/master/user.conf
+nginx -s reload
+
